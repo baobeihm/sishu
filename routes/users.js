4b9e8415410acda09ce4eeac0f6e1d91d779e8e3
Huan Chang <hhuanchang@gmail.com>
2017-01-19 21:05:21 -0800

    Add post method to users route. Create view.

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:name', function(req, res, next) {
+  res.render('users', { name: req.params.name });
+});
+
+router.post('/', function(req, res, next) {
+  var msg = req.body.message;
+  res.redirect('/users/' + msg);
 });
 
 module.exports = router;
